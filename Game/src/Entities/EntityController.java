c8a426b761228bb390f4c8324f2032883799b452
Edan Meyer <ninjaedan@aol.com>
2015-05-18 01:55:17 -0500

    Finished Game

--- Game/src/Entities/EntityController.java
+++
@@ -1,9 +0,0 @@
-package Entities;
-
-import java.util.ArrayList;
-
-public class EntityController {
-
-	public static ArrayList<Entity> entityList = new ArrayList<Entity>();
-	
-}
--- Game/src/Materials/Metal.java
+++
@@ -1,8 +0,0 @@
-package Materials;
-
-public class Metal extends ModelMaterial{
-	public Metal(){
-		shineDamper = 10;
-		reflectivity = 1;
-	}
-}
